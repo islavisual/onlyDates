3107f9bc34f2a30fecc7ff09fc328b7262559fe2
Isla Visual <islavisual@gmail.com>
2016-01-26 10:16:35 +0100

    Update onlyDates.js
    Added credits

--- onlyDates.js
+++ onlyDates.js
@@ -1,4 +1,7 @@
-
+// dataList 1.04 (https://github.com/islavisual/dataList).
+// Copyright 2015 Islavisual. Licensed under MIT (https://github.com/islavisual/dataList/blob/master/LICENSE). 
+// Author: Pablo E. Fernández (islavisual@gmail.com). 
+// Last update: 26/01/2016
 jQuery.fn.onlyDates   = function(options){
         var opt = $.extend({
             invalidDateMessage:'Invalid date',
